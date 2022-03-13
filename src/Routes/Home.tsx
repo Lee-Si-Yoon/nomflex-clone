463b9831c1e315bd498e3aa78c7f4184eecd8519
leesiyoon <76679207+Lee-Si-Yoon@users.noreply.github.com>
2022-03-14 08:51:39 +0900

    #8.8 Slider

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from "react-query";
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
 import { getMovies, IGetMovieResult } from "../api";
 import { makeImagePath } from "../utilities";
+import { useState } from "react";
 
 const Wrapper = styled.div``;
 
@@ -21,6 +23,7 @@ const Banner = styled.div<{ bgPhoto: string }>`
   background-image: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.8)),
     url(${(props) => props.bgPhoto});
   background-size: cover;
+  background-position: center center;
 `;
 
 const Title = styled.h2`
@@ -33,11 +36,61 @@ const OverView = styled.p`
   width: 50%;
 `;
 
+const Slider = styled(motion.div)`
+  position: relative;
+  top: -150px;
+`;
+
+const Row = styled(motion.div)`
+  position: absolute;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 5px;
+`;
+
+const Box = styled(motion.div)<{ bgPhoto: string }>`
+  background-color: white;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
+  height: 100px;
+  color: red;
+`;
+
+const rowVar = {
+  hidden: {
+    x: window.outerWidth + 5,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 5,
+  },
+};
+
+const offset = 6;
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMovieResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+  const increaseIndex = () => {
+    if (data) {
+      // this is for preventing double click
+      if (leaving) return;
+      toggleLeaving();
+      // because we used one movie in the banner
+      const totalMovies = data?.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+  const toggleLeaving = () => setLeaving((prev) => !prev);
   return (
     <Wrapper>
       {isLoading ? (
@@ -48,6 +101,28 @@ function Home() {
             <Title>{data?.results[0].title}</Title>
             <OverView>{data?.results[0].overview}</OverView>
           </Banner>
+          <Slider onClick={increaseIndex}>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              <Row
+                variants={rowVar}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                    ></Box>
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
